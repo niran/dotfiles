754b6359ce5d073f08c61ee72f21930289b8a93f
Niran Babalola <niran@niran.org>
2023-11-14 15:33:09 -0600

    Install the VS Code settings.json instead of the whole config directory, and guess the dotfiles path instead of hardcoding

--- install.sh
+++ install.sh
@@ -1,30 +1,22 @@
 #!/bin/bash
-#
-#  Derived from https://github.com/mattjmorrison/dotfiles
-#
-#===============================================================================
-#
-#             NOTES: For this to work you must have cloned the github
-#                    repo to your home folder as ~/dotfiles/
-#
-#===============================================================================
+# Derived from https://github.com/mattjmorrison/dotfiles
 
 #==============
 # Variables
 #==============
-dotfiles_dir=~/dotfiles
+dotfiles_dir=$(dirname "$(readlink -f $0)")
 
 #==============
 # Delete existing dot files and folders
 #==============
 sudo rm -rf ~/.gitconfig > /dev/null 2>&1
-sudo rm -rf ~/.config > /dev/null 2>&1
+sudo rm -rf ~/.config/Code/User/settings.json > /dev/null 2>&1
 sudo rm -rf ~/.vimrc > /dev/null 2>&1
 
 #==============
 # Create symlinks in the home folder
-# Allow overriding with files of matching names in the custom-configs dir
 #==============
-ln -s $dotfiles_dir/gitconfig ~/.gitconfig
-ln -sf $dotfiles_dir/vimrc ~/.vimrc
-ln -sf $dotfiles_dir/config ~/.config
+ln -s "$dotfiles_dir/gitconfig" ~/.gitconfig
+ln -sf "$dotfiles_dir/vimrc" ~/.vimrc
+mkdir -p "$dotfiles_dir/config/Code/User/"
+ln -sf "$dotfiles_dir/config/Code/User/settings.json" ~/.config/Code/User/settings.json
