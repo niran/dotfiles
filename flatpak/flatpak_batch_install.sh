7a3c63417932496bb960ba2c534a275c8fc33c1b
Niran Babalola <niran@niran.org>
2024-03-16 17:45:03 -0500

    Add GIMP and FileRoller flatpaks

--- flatpak/flatpak_batch_install.sh
+++ flatpak/flatpak_batch_install.sh
@@ -2,9 +2,11 @@ flatpak install -y \
   com.github.tchx84.Flatseal \
   de.haeckerfelix.Fragments \
   org.cryptomator.Cryptomator \
+  org.gimp.GIMP \
   org.gnome.Calculator \
   org.gnome.Evince \
   org.gnome.Evolution \
+  org.gnome.FileRoller \
   org.gnome.TextEditor \
   org.gnome.Weather \
   org.videolan.VLC 
